8ba173bc18b614418c8fecff79cc6fbb10574c3f
Sangeetha-xworkz <masangeetha13@gmail.com>
2024-07-18 23:20:46 +0530

    Delete SchoolBag.java

--- SchoolBag.java
+++
@@ -1,9 +0,0 @@
-class SchoolBag{
-      public static void main(String bag[]){
-      System.out.println("The backpack is LIGHT WEIGHT yet strong enough to hold your school supplies.");
-      System.out.println("The superior quality NYLON TWILL material lends a beautiful shine and soft feel to the backpack.");
-      System.out.println("Water Resistant, Warranty type: Manufacturer; 1 year manufacturer warranty is non-transferable and valid for 1 year from the original date of purchase.");
-      System.out.println("Ergonomic Straps: There is padded back mesh and ergonomic straps that gives extra spine and shoulder support and helps in minimizing the load after carrying the bag through a busy day at campus.");
-      System.out.println("EASY GRAB PADDED HANDLE for added comfort and convenience to hold the bag");
-      }  
-}
